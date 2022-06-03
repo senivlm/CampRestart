a33bec24e20313a9af629f1d3e288e4663824b03
AlonaPashko <alyona.pashko@gmail.com>
2022-06-03 10:13:16 +0200

    Organization

--- Task3/Program.cs
+++ Task3/Program.cs
@@ -2,30 +2,30 @@
 
 using Task3And4;
 
-//Vector vector1 = new Vector(20);
-//vector1.InitRandom(1,5);
+Vector vector1 = new Vector(20);
+vector1.InitRandom(1, 5);
 
-//Console.WriteLine(vector1);
+Console.WriteLine(vector1);
 
-//Pair[] pairs = vector1.CalcFrequency();
+Pair[] pairs = vector1.CalcFrequency();
 
-//for (int i = 0; i < pairs.Length; i++)
-//{
-//    Console.WriteLine(pairs[i] + " ");
-//}
-//Console.WriteLine();
-//Console.WriteLine("Longest frequency is: " + vector1.LongestFrequency(pairs));
+for (int i = 0; i < pairs.Length; i++)
+{
+    Console.WriteLine(pairs[i] + " ");
+}
+Console.WriteLine();
+Console.WriteLine("Longest frequency is: " + vector1.LongestFrequency(pairs));
 
-//Vector vector2 = new Vector(6);
-//vector2.InitShuffle();
+Vector vector2 = new Vector(6);
+vector2.InitShuffle();
 
-//Console.WriteLine(vector2);
+Console.WriteLine(vector2);
 
-//Vector vector3 = new Vector(10);
-//vector3.InitRandom(1, 20);
-//Console.WriteLine(vector3);
-//vector3.SplitMergeSort();
-//Console.WriteLine(vector3);
+Vector vector3 = new Vector(10);
+vector3.InitRandom(1, 20);
+Console.WriteLine(vector3);
+vector3.SplitMergeSort();
+Console.WriteLine(vector3);
 
 Vector vector4 = new Vector();
 vector4.ManualInitialisation();
