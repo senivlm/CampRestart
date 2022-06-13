54d8ea4b60a2eee9573a4f116e5baf8eb9153891
AlonaPashko <alyona.pashko@gmail.com>
2022-06-13 20:25:28 +0200

    Add some methods to task 6.2

---
+++ Task 6/Program.cs
@@ -0,0 +1,9 @@
+﻿
+using Task6;
+
+TextHandler text1 = new TextHandler("TextFile.txt");
+
+//text1.MakeSentence();
+
+text1.MakeWords();
+text1.WriteToFile("Result.txt");
---
+++ Task 6/TextHandler.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+namespace Task6
+{
+    internal class TextHandler
+    {
+        List<string> strings = new List<string>();
+
+        string line = "";
+
+        public TextHandler(string fileName)
+        {
+            try
+            {
+                StreamReader reader = new StreamReader(fileName);
+                line += reader.ReadToEnd();
+                reader.Close();
+            }
+            catch (FileNotFoundException)
+            {
+                Console.WriteLine("File not found");
+            }
+        }
+        public override string ToString()
+        {
+            return line;
+        }
+
+        public List<string> MakeSentence()
+        {
+            string[] array = line.Split('.');
+            for (int i = 0; i < array.Length; i++)
+            {
+                strings.Add(array[i]);
+                Console.WriteLine(strings[i] + ".");
+            }
+            return strings;
+        }
+        
+        public List<string> MakeWords()
+        {
+            string[] array = line.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            for (int i = 0; i < array.Length; i++)
+            {
+                strings.Add(array[i]);
+                Console.WriteLine(strings[i]);
+            }
+            return strings;
+        }
+        public void WriteToFile(string filePath)
+        {
+            StreamWriter writer = new StreamWriter(filePath);
+            if (!File.Exists(filePath))
+            {
+                File.Create(filePath);
+            }
+            for (int i = 0; i < strings.Count; i++)
+            {
+                writer.WriteLine(strings[i]);
+            }
+            writer.Close();
+        }
+
+        //public string RemoveExtraSpaces()
+        //{
+        //    String result = line;
+        //    String strResult = line.replaceAll("\\s+", " ").trim();
+        //}    
+    }
+}
