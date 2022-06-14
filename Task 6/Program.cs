5c9e893fab0e29020ac7339079975e875edbce10
AlonaPashko <alyona.pashko@gmail.com>
2022-06-14 10:48:38 +0200

    Task 6.2, but it has some bugs

--- Task 6/Program.cs
+++ Task 6/Program.cs
@@ -3,10 +3,9 @@ using Task6;
 
 TextHandler text1 = new TextHandler("TextFile.txt");
 
-//text1.MakeSentence();
-
-//text1.MakeWords();
-//text1.WriteToFile("Result.txt");
-
 text1.RemoveExtraSpaces();
-Console.WriteLine(text1);
+text1.MakeSentence();
+text1.WriteToFile("Result.txt");
+
+text1.PrintMinMaxWordColl();
+
--- Task 6/TextHandler.cs
+++ Task 6/TextHandler.cs
@@ -10,6 +10,12 @@ namespace Task6
     internal class TextHandler
     {
         List<string> strings = new List<string>();
+        
+        public List<string> Strings
+        {
+            get { return strings; }
+            set { strings = value; }
+        }
 
         string line = "";
 
@@ -31,13 +37,12 @@ namespace Task6
             return line;
         }
 
-        public List<string> MakeSentence()
+        public List<string> MakeSentence() //i think, it is another way here
         {
-            string[] array = line.Split('.');
+            string[] array = line.Split('.'); 
             for (int i = 0; i < array.Length; i++)
             {
                 strings.Add(array[i]);
-                Console.WriteLine(strings[i] + ".");
             }
             return strings;
         }
@@ -48,7 +53,6 @@ namespace Task6
             for (int i = 0; i < array.Length; i++)
             {
                 strings.Add(array[i]);
-                Console.WriteLine(strings[i]);
             }
             return strings;
         }
@@ -61,7 +65,7 @@ namespace Task6
             }
             for (int i = 0; i < strings.Count; i++)
             {
-                writer.WriteLine(strings[i]);
+                writer.WriteLine(strings[i] + ".");
             }
             writer.Close();
         }
@@ -82,5 +86,46 @@ namespace Task6
             line = stringBuilder.ToString();
             return line;
         }
+        public string MakeWordCollection(int index)
+        {
+            string sentence = strings[index];
+            return sentence;
+        }
+        public void FindMinMaxWord(string sentence) //it doesn't work correctly, MakeSentence is problem?
+        {
+            string[] words = sentence.Split(' ');
+            
+            int min = words[0].Length;
+            int max = words[0].Length;
+            
+            string longestWord = words[0];
+            string shortestWord = words[0];
+
+            for (int i = 0; i < words.Length; i++)
+            {
+                if (words[i].Length > max)
+                {
+                    longestWord = words[i];
+                    max = words[i].Length;
+                }
+                if (words[i].Length < min)
+                {
+                    shortestWord = words[i];
+                    min = words[i].Length;
+                }
+            }
+            Console.WriteLine("The longest word in sentence: " + longestWord + '\n' +
+                "The shortest word in sentence: " + shortestWord);
+        }
+        public void PrintMinMaxWordColl()
+        {
+            for (int i = 0; i < strings.Count; i++)
+            {
+                string temp = strings[i];
+                Console.WriteLine();
+                FindMinMaxWord(temp);
+            }
+        }
+        
     }
 }
